6e06177e0dd4a4aedffa909e2f59f7df62e36e95
Artur Bomtempo Colen <arturbcolen@gmail.com>
2026-02-22 21:50:58 -0300

    feat: update Header component layout and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,16 @@ import { Logo } from '../Logo';
 
 export function Header() {
     return (
-        <header className="flex h-22 items-center border-2 border-red-500">
+        <header className="flex h-22 items-center justify-around">
             <Logo />
+            <div className='flex gap-6 text-neutral-500 font-medium'>
+                <Link href="/" className='hover:border-b-3 border-teal-primary'>Home</Link>
+                <Link href="/indications">Indicações</Link>
+                <Link href="/about">Sobre nós</Link>
+                <Link href="/projects">Projetos</Link>
+                <Link href="/experiences">Experiencias</Link>
+                <Link href="/contact">Contato</Link>
+            </div>
         </header>
     );
 }
