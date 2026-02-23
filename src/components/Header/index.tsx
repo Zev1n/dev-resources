752ce9301a113d8ead120e9a401fa0f5a3fc8e26
Artur Bomtempo Colen <arturbcolen@gmail.com>
2026-02-22 21:42:09 -0300

    feat: update Header component styling and add Logo component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+import { Logo } from '../Logo';
+
 export function Header() {
     return (
-        <header>
-            Cabeçalho
+        <header className="flex h-22 items-center border-2 border-red-500">
+            <Logo />
         </header>
     );
 }
